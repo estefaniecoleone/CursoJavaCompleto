971df1a7d7dc75af09657e628b928f1d37eb7c58
Estefanie Coleone <84871976+estefaniecoleone@users.noreply.github.com>
2022-01-30 21:32:31 -0300

    Create EX2.java

---
+++ Section9/EX2.java
@@ -0,0 +1,19 @@
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
+
+import java.util.Scanner;
+
+class EX_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Digite um numero!");
+        numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("NUMERO PAR!!!");
+        } else {
+            System.out.println("NUMERO IMPAR!!!");
+        }
+        sc.close();
+    }
+}
